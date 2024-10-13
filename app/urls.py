9d703dd5555b60ef5b61fb5c1029a4aa9f616e51
Breno Pereira <brenopereirabetti@gmail.com>
2024-10-12 21:35:00 -0300

    :sparkles: feat: creates route for logout

--- app/urls.py
+++ app/urls.py
@@ -13,6 +13,7 @@ def home_view(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', views.home, name='home'),
     path('', include('brands.urls')),
